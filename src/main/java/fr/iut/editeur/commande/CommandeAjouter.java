65bf9cb85c34e43bcddb6022ff4e5dc70ba3b5fd
Julien Costa castro <julien.costa-castro@etu.umontpellier.fr>
2023-11-14 14:30:30 +0100

    javadoc

--- src/main/java/fr/iut/editeur/commande/CommandeAjouter.java
+++ src/main/java/fr/iut/editeur/commande/CommandeAjouter.java
@@ -1,17 +1,30 @@
 package fr.iut.editeur.commande;
 
+import fr.iut.editeur.commande.CommandeDocument;
 import fr.iut.editeur.document.Document;
 
+/**
+ * Cette classe représente une commande pour ajouter du texte à un document.
+ * Elle hérite de la classe abstraite CommandeDocument.
+ */
 public class CommandeAjouter extends CommandeDocument {
 
-
+    /**
+     * Constructeur de la classe CommandeAjouter.
+     * @param document Le document sur lequel la commande doit être exécutée.
+     * @param parameters Les paramètres de la commande.
+     */
     public CommandeAjouter(Document document, String[] parameters) {
         super(document, parameters);
     }
 
+    /**
+     * Méthode pour exécuter la commande d'ajout de texte.
+     * Si le format des paramètres n'est pas respecté, un message d'erreur est affiché.
+     */
     @Override
     public void executer() {
-        if(parameters.length < 2) {
+        if (parameters.length < 2) {
             System.err.println("Format attendu : ajouter;texte");
             return;
         }
--- src/main/java/fr/iut/editeur/commande/CommandeDocument.java
+++ src/main/java/fr/iut/editeur/commande/CommandeDocument.java
@@ -2,19 +2,39 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
+/**
+ * Cette classe est une classe abstraite qui représente une commande sur un document.
+ * Elle implémente l'interface Commande.
+ */
 public abstract class CommandeDocument implements Commande {
 
+    /**
+     * Le document sur lequel la commande doit être exécutée.
+     */
     protected Document document;
 
-    protected String[]parameters;
+    /**
+     * Les paramètres de la commande.
+     */
+    protected String[] parameters;
 
+    /**
+     * Constructeur de la classe CommandeDocument.
+     * @param document Le document sur lequel la commande doit être exécutée.
+     * @param parameters Les paramètres de la commande.
+     */
     public CommandeDocument(Document document, String[] parameters) {
         this.document = document;
         this.parameters = parameters;
     }
 
+    /**
+     * Méthode pour exécuter la commande.
+     * Par défaut, elle affiche le contenu du document.
+     */
     @Override
     public void executer() {
         System.out.println(this.document);
     }
 }
+
